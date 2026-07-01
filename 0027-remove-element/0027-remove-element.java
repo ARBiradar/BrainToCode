62bf07ec70182ef23ef2e9b933aadfadc4a70395
Adarsh Biradar <129952645+ARBiradar@users.noreply.github.com>
2026-07-01 21:50:31 +0530

    Create README - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
-         int k = 0; // index for placing non-val elements
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+        int count = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
             }
         }
-        return k;
+        return count;
+        
     }
-    
 }
