7646db3136b0a77c416eac3aa921b5d974a781ba
Adarsh Biradar <129952645+ARBiradar@users.noreply.github.com>
2026-08-07 00:41:55 +0530

    Add GeeksForGeeks solution: Array Leaders

---
+++ GeeksForGeeks/Array-Leaders/solution.cpp
@@ -0,0 +1,19 @@
+import java.util.Collections;
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        int max = 0;
+        for(int i = arr.length -1; i>=0; i--){
+            if(arr[i] > max){
+                al.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        al.sort( (a, b) -> { return -1 * a.compareTo(b); } );
+        
+        return al;
+    }
+    
+    
+}
